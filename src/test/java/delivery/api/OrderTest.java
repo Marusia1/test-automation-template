20eb605d4577a55882980450cadd252d12ce6910
Marija <gestuverteja.marija@gmail.com>
2024-03-30 10:45:08 +0200

    add readme file 17 homework

--- src/test/java/delivery/api/OrderTest.java
+++ src/test/java/delivery/api/OrderTest.java
@@ -1,8 +1,6 @@
 package delivery.api;
 
 import delivery.dto.OrderDto;
-import delivery.dto.OrderDtoBuilderAndFactory;
-import delivery.utils.RandomDataGenerator;
 import io.restassured.response.Response;
 import org.apache.http.HttpStatus;
 import org.junit.jupiter.api.Assertions;
@@ -15,26 +13,35 @@ public class OrderTest extends BaseSetupApi {
     @Test
     void getOrderInformationAndCheckResponse() {
 
-        Response response = ApiClient.getOrders(getAuthenticatedRequestSpecification() );
+        Response response = ApiClient.getOrders(getAuthenticatedRequestSpecification());
 
         Assertions.assertAll("Test description",
                 () -> Assertions.assertEquals(HttpStatus.SC_OK, response.getStatusCode(), "Status code is OK")
         );
 
     }
+
     // 14 homework. Test for new order creation
     @Test
     void createOrderAndCheckResponse() {
-      // order creation (may use factory) -> replace new OrderDto???? correctly initialize new OrderDto
-       Response response = ApiClient.createOrder(getAuthenticatedRequestSpecification(), new OrderDto() );
-       // Response response = ApiClient.createOrder(getAuthenticatedRequestSpecification(), new OrderDtoBuilderAndFactory() );
 
-
+        Response response = ApiClient.createRandomOrder(getAuthenticatedRequestSpecification());
 
 
         Assertions.assertAll("Test description",
-                () -> Assertions.assertEquals(HttpStatus.SC_OK, response.getStatusCode(), "Status code is OK")
+                () -> Assertions.assertEquals(HttpStatus.SC_OK, response.getStatusCode(), "Status code is OK"),
+                () -> Assertions.assertNotNull(response.getBody().path("id"))
         );
 
     }
+
+    // 15 Homework. Delete method
+    @Test
+    void deleteOrderById() {
+        Response response = ApiClient.createRandomOrder(getAuthenticatedRequestSpecification());
+        int orderId = response.getBody().path("id");
+
+        Response responseOrderDeletion = ApiClient.deleteOrderById(getAuthenticatedRequestSpecification(), orderId);
+    }
 }
+
--- src/test/java/delivery/dto/OrderDto.java
+++ src/test/java/delivery/dto/OrderDto.java
@@ -1,76 +1,31 @@
 package delivery.dto;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.Setter;
 
-
+import delivery.utils.RandomDataGenerator;
+@Builder
+@Getter
+@Setter
 public class OrderDto {
     private String status;
     private int courierId;
     private String customerName;
     private String customerPhone;
     private String comment;
-    private long id;
+    private int id;
 
 
-
-    public OrderDto(String status, int courierId, String customerName, String customerPhone, String comment, int id) {
-        this.status = status;
-        this.courierId = courierId;
-        this.customerName = customerName;
-        this.customerPhone = customerPhone;
-        this.comment = comment;
-        this.id = id;
-    }
-
-
-    public String getStatus() {
-        return status;
-    }
-
-    public OrderDto() {
-
-    }
-
-    public int getCourierId() {
-        return courierId;
-    }
-
-    public String getCustomerName() {
-        return customerName;
-    }
-
-    public String getCustomerPhone() {
-        return customerPhone;
-    }
-
-    public String getComment() {
-        return comment;
-    }
-
-    public long getId() {
-        return id;
-    }
-
-    public void setStatus(String status) {
-        this.status = status;
-    }
-
-    public void setCourierId(int courierId) {
-        this.courierId = courierId;
-    }
-
-    public void setCustomerName(String customerName) {
-        this.customerName = customerName;
-    }
-
-    public void setCustomerPhone(String customerPhone) {
-        this.customerPhone = customerPhone;
-    }
-
-    public void setComment(String comment) {
-        this.comment = comment;
-    }
-
-    public void setId(long id) {
-        this.id = id;
+    public static OrderDto createRandomOrder() {
+        // builder
+        return OrderDto.builder()
+                .status("OPEN")
+                .courierId(0)
+                .customerName(RandomDataGenerator.generateName())
+                .customerPhone("12343456")
+                .comment("comment")
+                .id(1)
+                .build();
     }
 }
 
